409541663339f4ad53f4e7131f9947f9e85df31f
loumt <loumt@sanlogic.com>
2017-08-15 15:26:20 +0800

    新增用户类

---
+++ src/main/java/com/mooty/pojo/User.java
@@ -0,0 +1,59 @@
+package com.mooty.pojo;
+
+import java.util.Date;
+
+/**
+ * Created by loumt on 2017/8/15.
+ */
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private Date createDate;
+
+    private Date modifyDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
