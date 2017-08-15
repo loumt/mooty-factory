1a96f78eab8fa0d29db2bd45b86e1db894e174cd
loumt <loumt@sanlogic.com>
2017-08-15 16:34:52 +0800

    新增权限类

---
+++ src/main/java/com/mooty/pojo/Process.java
@@ -0,0 +1,17 @@
+package com.mooty.pojo;
+
+/**
+ * Created by loumt on 2017/8/15.
+ */
+public class Process {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
