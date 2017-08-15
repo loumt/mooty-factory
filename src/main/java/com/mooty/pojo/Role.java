e2cceadc77b5d2cb1b6600b8ddacfddb220f27dd
loumt <loumt@sanlogic.com>
2017-08-15 15:28:27 +0800

    新增角色类

---
+++ src/main/java/com/mooty/pojo/Role.java
@@ -0,0 +1,69 @@
+package com.mooty.pojo;
+
+import java.util.Date;
+
+/**
+ * Created by loumt on 2017/8/15.
+ */
+public class Role {
+
+    private Long id;
+
+    private String code;
+
+    private String roleName;
+
+    private Long createUserId;
+
+    private Date createDate;
+
+    private Date modifyDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Long getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Long createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
